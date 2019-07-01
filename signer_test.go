0d6c037dce0ca43c88868d65545e60b894d81405
oskar <oskar@paolini.pl>
2019-07-01 20:58:46 +0800

    adds test from previous commits

--- signer_test.go
+++ signer_test.go
@@ -17,6 +17,33 @@ const signerData = `
 {"@timestamp":"2019-06-30T11:07:15.000Z", "source":"stderr", "log":"{\"error\":\"Post http://ganache:8545: EOF\",\"level\":\"error\",\"msg\":\"BlockWatcher error encountered\",\"time\":\"2019-06-30T11:07:15Z\"}", "container_id":"b33055fd8054ad060d167da59798621a126093582e31c3c273ad2ddff928cc1b", "container_name":"/docker-compose-shipper_mesh_1"}
 `
 
+func TestSignAndAuth(t *testing.T) {
+	signer, err := NewP2PSignerFromKeyPath("./__fixtures__/privkey")
+	if err != nil {
+		t.Fatal(err)
+	}
+	body := ioutil.NopCloser(bytes.NewReader([]byte(signerData)))
+	request := httptest.NewRequest("POST", "/", body)
+
+	err = signer.SignRequest(request)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	allowedIDs := []string{"036a775c4db73fd351191d0a0e19862ecbb70cbe2626097adfb70ffd4f9ea081bf"}
+	auth, err := NewP2PAuthenticator(allowedIDs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ok := auth.AuthenticateRequest(request)
+	if !ok {
+		t.Fatal("did not properly authenticate the request")
+	}
+
+}
+
 func TestSerializeAndDeserialize(t *testing.T) {
 	pubKeyHex := "036a775c4db73fd351191d0a0e19862ecbb70cbe2626097adfb70ffd4f9ea081bf"
 	pubBytes, err := hex.DecodeString(pubKeyHex)
